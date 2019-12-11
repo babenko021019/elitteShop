7ad9cf03d5ca4e86895850174172ef92d7f4e123
Admin <Admin@DESKTOP-VHGS4I3>
2019-12-11 19:20:11 +0200

    Initial commit

---
+++ src/test/java/com/mainacad/dao/UserDaoTest.java
@@ -0,0 +1,21 @@
+package com.mainacad.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class UserDaoTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	
+	void test() {
+		assertTrue(true);
+//		fail("Not yet implemented");
+	}
+
+}
